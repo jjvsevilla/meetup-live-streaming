5febf70bbe760645ebdae32927c4c64fd7d7530c
Juan Vento <j.j.v.sevilla@gmail.com>
2019-09-09 21:41:46 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import reactLogo from "./assets/react.svg";
+import agGridLogo from "./assets/ag-grid.png";
+import "./App.scss";
+
+const openNewWindow = url => window.open(url, "_blank");
 
 function App() {
+  const goToReact = () =>
+    openNewWindow("https://create-react-app.dev/docs/getting-started");
+
+  const goToAgGrid = () =>
+    openNewWindow("https://www.ag-grid.com/javascript-grid-viewport/");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="logos">
+          <button onClick={goToReact}>
+            <img src={reactLogo} className="logo" alt="React" />
+          </button>
+          <button onClick={goToAgGrid}>
+            <img src={agGridLogo} className="logo" alt="Ag-Grid" />
+          </button>
+        </div>
+        <p>Handling live streaming updates with javascript</p>
       </header>
     </div>
   );
